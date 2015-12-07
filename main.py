c395d780d5d7190e583e8878b6da0b9a0ba188c0
kadhir arauz <kad16sil@gmail.com>
2015-12-07 04:39:32 -0500

    bienvenida
    inclui un pequeño video dando bienvenida a la app pero no se como hacer
    que aparesca antes de toda la informacion de la app

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
